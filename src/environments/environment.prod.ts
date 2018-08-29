df866d4c71c69fb44a080d31c5eeac0edfb3c82e
Evgeniy Konstantinov <Konstantinov-91@mail.ru>
2018-08-29 13:29:32 +1000

    enironment edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://www.omdbapi.com/',
+  apiKey: 'ff000a85',
+  apiGetOneFilm: '?i=',
+  apiGetMultyFilms: '?s=',
+  localStorageKey: 'films_store'
 };
